0c644be53bd8c5b5a72055c2188549aa866678e5
AndreyTepin <47591725+AndreyTepin@users.noreply.github.com>
2020-09-29 12:35:15 +0300

    Add files via upload

---
+++ lab3_3/lab3_3/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace lab3_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_Brezenheim = new System.Windows.Forms.Button();
+            this.button_VU = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(618, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button_Brezenheim
+            // 
+            this.button_Brezenheim.Enabled = false;
+            this.button_Brezenheim.Location = new System.Drawing.Point(648, 12);
+            this.button_Brezenheim.Name = "button_Brezenheim";
+            this.button_Brezenheim.Size = new System.Drawing.Size(123, 23);
+            this.button_Brezenheim.TabIndex = 1;
+            this.button_Brezenheim.Text = "Брезенхейм";
+            this.button_Brezenheim.UseVisualStyleBackColor = true;
+            this.button_Brezenheim.Click += new System.EventHandler(this.Change_Method);
+            // 
+            // button_VU
+            // 
+            this.button_VU.Location = new System.Drawing.Point(648, 73);
+            this.button_VU.Name = "button_VU";
+            this.button_VU.Size = new System.Drawing.Size(123, 23);
+            this.button_VU.TabIndex = 2;
+            this.button_VU.Text = "ВУ";
+            this.button_VU.UseVisualStyleBackColor = true;
+            this.button_VU.Click += new System.EventHandler(this.Change_Method);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(648, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_VU);
+            this.Controls.Add(this.button_Brezenheim);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_Brezenheim;
+        private System.Windows.Forms.Button button_VU;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ lab3_3/lab3_3/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3_3
+{
+    public partial class Form1 : Form
+    {
+        int size_ellipse_point = 6; //Размер эллипса в точке начала и конца отрезка
+        Queue<Point> points = new Queue<Point>();
+        Color colorLine = Color.Black;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Change_Method(object sender, EventArgs e)
+        {
+            Button cur_button = (Button)sender;
+            cur_button.Enabled = false;
+            if (cur_button == button_Brezenheim)
+            {
+                button_VU.Enabled = true;
+            }
+            else
+            {
+                button_Brezenheim.Enabled = true;
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point cur_point = e.Location;
+            Graphics g = pictureBox1.CreateGraphics();
+
+            
+
+            if (points.Count == 2)
+            {
+                points.Dequeue();
+            }
+
+            points.Enqueue(cur_point);
+            g.FillEllipse(new SolidBrush(Color.Red), cur_point.X - size_ellipse_point/2, cur_point.Y - size_ellipse_point/2, size_ellipse_point, size_ellipse_point);
+
+            if (points.Count == 2)
+            {
+                Point point1 = points.Dequeue();
+                Point point2 = points.Peek();
+
+                if (!button_Brezenheim.Enabled)
+                {
+                    Brezenheim(g, point1, point2);
+                }
+                else
+                {
+                    VU(g, point1, point2);
+                }
+            }
+        }
+
+        private void Brezenheim(Graphics g, Point point1, Point point2)
+        {
+            int distanceX = Math.Abs(point2.X - point1.X);
+            int distanceY = Math.Abs(point2.Y - point1.Y);
+            double grad = distanceY / (double)distanceX;
+            Tuple<int, int> dirXY = Direction(point1, point2);
+
+            
+            int x = point1.X;
+            int y = point1.Y;
+            Bitmap pixel = new Bitmap(1,1);
+            pixel.SetPixel(0, 0, colorLine);
+            if (grad <= 1)
+            {
+                double d = 2 * distanceY - distanceX;
+                while (x != point2.X || y != point2.Y)
+                {
+                    if (d <= 0)
+                    {
+                        d = d + 2 * distanceY;
+                    }
+                    else
+                    {
+                        y += dirXY.Item2;
+                        d = d + 2 * (distanceY - distanceX);
+                    }
+
+                    x += dirXY.Item1;
+                    g.DrawImageUnscaled(pixel, x, y);
+                }
+            }
+            else
+            {
+                double d = 2 * distanceX - distanceY;
+                while (x != point2.X || y != point2.Y)
+                {
+                    if (d < 0)
+                    {
+                        d = d + 2 * distanceX;
+                    }
+                    else
+                    {
+                        x += dirXY.Item1;
+                        d = d + 2 * (distanceX - distanceY);
+                    }
+
+                    y += dirXY.Item2;
+                    g.DrawImageUnscaled(pixel, x, y);
+                }
+            }
+        }
+
+        private Tuple<int, int> Direction(Point point1, Point point2)
+        {
+            int x = 0;
+            int y = 0;
+
+            if (point1.X > point2.X)
+            {
+                x = -1;
+            }
+            else if (point1.X < point2.X)
+            {
+                x = 1;
+            }
+
+
+            if (point1.Y > point2.Y)
+            {
+                y = -1;
+            }
+            else if (point1.Y < point2.Y)
+            {
+                y = 1;
+            }
+
+            return new Tuple<int, int>(x, y);
+        }
+
+        private void DrawPoint(Graphics graphics, bool steep, int x, int y, double grade)
+        {
+            grade = 1 - grade;
+            Bitmap bm = new Bitmap(1, 1);
+            bm.SetPixel(0, 0, Color.FromArgb((int)(255 * grade), (int)(255 * grade), (int)(255 * grade)));
+            if (steep)
+                graphics.DrawImageUnscaled(bm, y, x);
+            else
+                graphics.DrawImageUnscaled(bm, x, y);
+        }
+
+        private void Swap(ref int a, ref int b)
+        {
+            int t = a;
+            a = b;
+            b = t;
+        }
+        private void VU(Graphics g, Point start, Point finish)
+        {
+            int x0 = start.X;
+            int y0 = start.Y;
+
+            int x1 = finish.X;
+            int y1 = finish.Y;
+
+            bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+            if (steep)
+            {
+                Swap(ref x0, ref y0);
+                Swap(ref x1, ref y1);
+            }
+            if (x0 > x1)
+            {
+                Swap(ref x0, ref x1);
+                Swap(ref y0, ref y1);
+            }
+
+
+
+            DrawPoint(g, steep, x0, y0, 1);
+            DrawPoint(g, steep, x1, y1, 1);
+            float dx = x1 - x0;
+            float dy = y1 - y0;
+            float gradient = dy / dx;
+            float y = y0 + gradient;
+            for (var x = x0 + 1; x <= x1 - 1; x++)
+            {
+                DrawPoint(g, steep, x, (int)y, 1 - (y - (int)y));
+                DrawPoint(g, steep, x, (int)y + 1, y - (int)y);
+                y += gradient;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(SystemColors.Control);
+
+        }
+    }
+
+}
